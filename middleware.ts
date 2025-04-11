06371994430a1b8cf9ba114a44bc253e5c6bfbf1
Aastik001 <aastik1000@gmail.com>
2025-04-11 18:35:30 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,30 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware({
-    publicRoutes: [
-        "/",
-        "/events/:id",
-        "/api/webhook/clerk",
-        "/api/webhook/stripe",
-        "/api/uploadthing",
-    ],
-    ignoredRoutes: [
-        "/api/webhook/clerk",
-        "/api/webhook/stripe",
-        "/api/uploadthing",
-    ]
-    
+const isPublicRoute = createRouteMatcher([
+  '/',
+  '/events/:id',
+  '/api/webhook/clerk',
+  '/api/webhook/stripe',
+  '/api/uploadthing',
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isPublicRoute(req)) {
+    return; // Allow access
+  }
+
+  const { userId } = await auth();
+
+  if (!userId) {
+    // Redirect unauthenticated users
+    return Response.redirect(new URL('/sign-in', req.url));
+  }
+
+  // Allow access for signed-in users
 });
-
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
+    '/((?!_next|favicon.ico|.*\\..*).*)',
     '/(api|trpc)(.*)',
   ],
 };
