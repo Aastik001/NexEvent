20d045fe49f12fe0f3e637bfc1e513459a4840ec
Aastik001 <aastik1000@gmail.com>
2025-04-12 17:56:17 +0530

    Update user.model.ts

--- lib/mongodb/database/models/user.model.ts
+++ lib/mongodb/database/models/user.model.ts
@@ -4,9 +4,9 @@ const UserSchema = new Schema({
     clerkId: {type:String, required:true, unique:true},
     email:{type:String, required:true, unique:true},
     username:{type:String, required:true, unique:true},
-    firstname:{type:String, required:true},
-    lastname:{type:String, required:true},
-    photo:{type:String, required:true},
+    firstname:{type:String, required:false},
+    lastname:{type:String, required:false},
+    photo:{type:String, required:false},
 })
 const User = models.User || model("User", UserSchema);
 export default User;
