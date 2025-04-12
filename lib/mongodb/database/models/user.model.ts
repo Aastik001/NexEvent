34576114bf98d6389776796731ccb4e47c876d63
Aastik001 <aastik1000@gmail.com>
2025-04-12 17:59:44 +0530

    Update user.model.ts

--- lib/mongodb/database/models/user.model.ts
+++ lib/mongodb/database/models/user.model.ts
@@ -3,7 +3,7 @@ import { model, models, Schema } from "mongoose";
 const UserSchema = new Schema({
     clerkId: {type:String, required:true, unique:true},
     email:{type:String, required:true, unique:true},
-    username:{type:String, required:true, unique:true},
+    username:{type:String, required:false, unique:true},
     firstname:{type:String, required:false},
     lastname:{type:String, required:false},
     photo:{type:String, required:false},
